5a796570d2c2d2cfaa17a407390c0438bdefdc0d
yamamoto1108 <kjrps0826@gmail.com>
2019-04-11 15:28:11 +0900

    Add association

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   def show
+    @name = current_user.name
+    @about = current_user.about
+    #@posts = Post.where(user_id: current_user.id).page(params[:page]).per(5).order("created_at DESC")
   end
 
   def edit
