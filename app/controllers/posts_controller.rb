c9067e32b3452fbdb5514d4c274c7081a79615a1
yamamoto1108 <kjrps0826@gmail.com>
2019-04-11 16:45:01 +0900

    Add file_field

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,19 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text, :image).merge(user_id: current_user.id)
+  end
 end
