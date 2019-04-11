e660f35e0dccb0bfab84c5e115800bed65b3373f
yamamoto1108 <kjrps0826@gmail.com>
2019-04-11 13:03:04 +0900

    Set show, edit, update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if current_user.update(user_params)
+      redilect_to user_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :about)
+  end
 end
