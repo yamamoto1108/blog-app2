267924b644dea7f2e17cad2f25431a2a7052c2b8
yamamoto1108 <kjrps0826@gmail.com>
2019-04-12 19:23:23 +0900

    Set post edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,9 +10,23 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     if @post.save
-      redirect_to root_path
+      redirect_to root_path, notice: "投稿しました"
     else
       render :new
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    if post.user_id == current_user.id
+      post.update(post_params)
+      redirect_to root_path, notice: "更新しました"
+    else
+      render :edit
     end
   end
 
