68e8a1f0b8a81e9a08174df397e4c1b5dd86d6d4
yamamoto1108 <kjrps0826@gmail.com>
2019-04-15 15:00:32 +0900

    Change ban design

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,2 +1,7 @@
 class TagsController < ApplicationController
+  def show
+    @tag = Tag.find(params[:id])
+    @posts = @tag.posts.page(params[:page]).per(5).order("created_at DESC")
+    @tags = Tag.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments, only: [:new, :create]
   end
+  resources :tags, only: [:show]
   post "/like/:post_id" => "likes#like", as: 'like'
   delete "/like/:post_id" => "likes#unlike", as: 'unlike'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
