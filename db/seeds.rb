245f7e6bb08a62a30c926c61a3f96aa63fad319a
yamamoto1108 <kjrps0826@gmail.com>
2019-04-15 12:13:24 +0900

    Set tags

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Tag.create(tag: 'グルメ、料理')
+Tag.create(tag: '旅行')
+Tag.create(tag: 'ファッション')
+Tag.create(tag: '暮らし')
+Tag.create(tag: '趣味')
+Tag.create(tag: '仕事')
+Tag.create(tag: 'ニュース')
+Tag.create(tag: 'エンタメ')
+Tag.create(tag: 'その他')
