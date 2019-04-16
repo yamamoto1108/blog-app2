af785b1c538d718684da48e0982f382cdd4c7844
yamamoto1108 <kjrps0826@gmail.com>
2019-04-16 20:28:59 +0900

    Add association

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :like_users, through: :likes, source: :user
   has_many :likes, dependent: :destroy
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   has_many :post_tags
   has_many :tags, through: :post_tags
