6794fc8b1635779fb16fed44b7df5d4e3288faaa
yamamoto1108 <kjrps0826@gmail.com>
2019-04-20 19:12:49 +0900

    Add association for post_tags

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
 
-  has_many :post_tags
+  has_many :post_tags, dependent: :destroy
   has_many :tags, through: :post_tags
 
   mount_uploader :image, ImageUploader
